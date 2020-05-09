53c1c9a845f8423296407d0ec5e25dfc777dcfef
Alejandro F <afuste7@gmail.com>
2020-05-08 21:05:28 -0400

    added table and headers to TableHeaders component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Container from './components/Container';
 import Header from './components/Header';
 import Search from './components/Search';
+import TableHeaders from './components/TableHeaders';
 
 function App() {
-	return <Container>{[ <Header />, <Search /> ]}</Container>;
+	return <Container>{[ <Header />, <Search />, <TableHeaders /> ]}</Container>;
 }
 
 export default App;
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 function Container(props) {
-	return <div className="container-fluid">{props.children}</div>;
+	return <div className="container-fuild">{props.children}</div>;
 }
 
 export default Container;
